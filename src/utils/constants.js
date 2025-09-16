8836a86fca152e0729baaaf4ddd0f58ef058d490
Dinchan.dev <dinhtruonghioo4@gmail.com>
2025-09-16 10:06:20 +0700

    Fix Router

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,10 @@
 // export const API_ROOT = 'http://localhost:8017'
 let apiRoot = ''
+// eslint-disable-next-line no-undef
 if(process.env.BUILD_MODE === 'dev'){
   apiRoot= 'http://localhost:8017'
 }
+// eslint-disable-next-line no-undef
 if(process.env.BUILD_MODE === 'production'){
   apiRoot = 'https://trello-api-4837.onrender.com'
 }
